27b9bf55594121b46e381a5fa4f1c85d10e9ecbb
konradstanczyk <115588466+konradstanczyk@users.noreply.github.com>
2022-10-11 22:58:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var org_en = ["Certificate organizer", "strefakursow.pl", "strefakursow.pl"];
 var tra_pl = ["Szkolenie", "Ekosystem JavaScript - wprowadzenie", "ES6 nowa generacja JavaScript"];
 var tra_en = ["Training", "The JavaScript ecosystem - introduction", "ES6 new generation of JavaScript"];
 
-var date_pl = ["Data ukończenia", "09.09.2022", "-"];
-var date_en = ["Date of acquiring", "09.09.2022", "-"];
+var date_pl = ["Data ukończenia", "09.09.2022", "10.10.2022"];
+var date_en = ["Date of acquiring", "09.09.2022", "10.10.2022"];
 
 var textBox_pl = "<span class='whitetext'>Cześć</span>, mam na imię <span class='whitetext'>Konrad</span>, od dłuższego czasu pasjonuję się programowaniem stron internetowych oraz aplikacji mobilnych. Obecnie jestem studentem 3 roku Informatyki Stosowanej na Uniwersytecie Łódzkim i aktywnie poszukuję pracy jako Junior JavaScript Developer.";
 var textBox_en = "<span class='whitetext'>Hi</span>, my name is <span class='whitetext'>Konrad</span>, I have been passionate about web and mobile application programming for a long time. I am currently a 3rd year student of Applied Informatics at the University of Lodz and am actively looking for a job as a Junior JavaScript Developer.";
